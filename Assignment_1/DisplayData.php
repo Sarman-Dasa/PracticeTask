40d6158a0125c89bd0d5f69743a1ac88e759a01b
sarmandasa <sarmand@zignuts.com>
2023-01-27 19:34:41 +0530

    format Edit Delete Button

--- Assignment_1/DisplayData.php
+++ Assignment_1/DisplayData.php
@@ -1,31 +1,34 @@
 <?php
 include_once('Connction.php');
 include_once('/../Assignment_1/css');
-$query = "select * from TODU_TBL";
-$result = mysqli_query($con, $query);
+    $query = "select * from TODU_TBL";
+    $result = mysqli_query($con, $query);
 ?>
 <html>
 
 <head>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    
 </head>
 
 <body>
     <div class="container">
-        <table class="table table-responsive table-striped">
+        <table class="table table-responsive table-striped ">
             <thead>
                 <tr>
                     <th class="text-center text-primary active" colspan="4">TUDO TABLE DATA</th>
                 </tr>
-                <tr class="bg-primary">
+               
+            <?php
+            if (mysqli_num_rows($result) > 0) {?>
+             <tr class="bg-primary">
                     <th>ID</th>
                     <th>TITLE</th>
                     <th>DESCRIPTION</th>
-                    <th>EDIT/DELETE</th>
+                    <th class="text-center">ACTION</th>
                 </tr>
             </thead>
             <?php
-            if (mysqli_num_rows($result) > 0) {
 
                 while ($row = mysqli_fetch_row($result)) {
                     ?>
@@ -34,42 +37,51 @@ $result = mysqli_query($con, $query);
                             <td><?php echo $row[1] ?></td>
                             <td><?php echo $row[2] ?></td>
                          
-                            <td><?php echo "<button id='delete_ID' data-id='{$row[0]}'>Delete</button>" ?>
-                           <?php echo "<a href='DeleteData.php?id=$row[0]' id='deleteID'>DELETE</a>" ?></td>
+                            <td class="text-center">
+                              
+                                <?php echo "<a href='index.php?id=$row[0]' id='editID'>
+                                <span class='btn-lg glyphicon glyphicon-pencil text-decoration-none' style='color:green;font-size:18px;'>
+                                </span></a>" ?> 
+                                <?php echo "<span id='delete_ID' data-id='{$row[0]}'  class='btn-lg glyphicon glyphicon-trash'></span>" ?>
+                                <?php echo "<span id='delete_ID' data-id='{$row[0]}' class='btn-lg glyphicon glyphicon-remove-sign' 
+                                data-toggle='tooltip' data-placement='top' title='Tooltip on top'></span>" ?>
+                            </td>
+                          <?php //echo "<button id='editID' data-id='{$row[0]}'> EDIT </button>" ?>
                         </tr>
                     <?php
                 }
-            } else {
-                echo "No Data Found!!!";
-            }
+            } else { ?>
+                <tr>
+                    <td colspan="4" class="text-center">
+                        <p class="text-danger"> No Data Found!!! </p>
+                    </td>
+                </tr>
+           <?php }
             ?>
         </table>
     </div>
-    <script>
-       
-            $(document).on("click","#delete_ID",function(e)
-            {
-               if(confirm("Do you want to Delete This Record")){
-                var title_id = $(this).data("id"); 
-                var element = this;
-                $.ajax({
-                    url : 'DeleteData.php',
-                    type : "POST",
-                    data : {id:title_id},
-                    success: function(data){
-                      if(data==1)
-                      {
-                        $(element).closest("tr").fadeOut(1000); // Just Remove a tr // table can not refrech so design remove
-                      }
-                      else{
+    <!-- <script> 
+         // $(document).on("click","#delete_ID",function(e)
+            // {
+            //    if(confirm("Do you want to Delete This Record")){
+            //     var title_id = $(this).data("id"); 
+            //     var element = this;
+            //     $.ajax({
+            //         url : 'DeleteData.php',
+            //         type : "POST",
+            //         data : {id:title_id},
+            //         success: function(data){
+            //           if(data==1)
+            //           {
+            //             $(element).closest("tr").fadeOut(1000); // Just Remove a tr // table can not refrech so design remove
+            //           }
+            //           else{
                         
-                      }
-                    }
-                });
-               }
-            })
-      
-    </script>
+            //           }
+            //         }
+            //     });
+            //    }
+            // })  </script> -->
 </body>
 
 </html>
