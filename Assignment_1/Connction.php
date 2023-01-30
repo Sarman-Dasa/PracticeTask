e64829340861f01c85ff34f07b2d7e5152e0a3a1
sarmandasa <sarmand@zignuts.com>
2023-01-30 15:09:19 +0530

    Change Connection database

--- Assignment_1/Connction.php
+++ Assignment_1/Connction.php
@@ -1,7 +1,7 @@
 <?php
    try{
         error_reporting(0);
-        $con = mysqli_connect('localhost','root','root','Assignment_1');
+        $con = mysqli_connect('localhost','root','root','practicetask');
         if(mysqli_connect_error())
         {
             throw new Exception('Connection Failed..');
