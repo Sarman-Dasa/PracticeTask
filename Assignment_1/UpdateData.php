9e6f8a0d8d5db6078c34789138b1d5fb5081f647
sarmandasa <sarmand@zignuts.com>
2023-01-30 15:12:23 +0530

    change Query string table name

--- Assignment_1/UpdateData.php
+++ Assignment_1/UpdateData.php
@@ -1,16 +1,29 @@
 <?php
     include_once("Connction.php");
-    $id = $_POST['id'];
-    $title = $_POST['title'];
-    $description = $_POST['description'];
+    if(isset($_POST['id']))
+    {
+        $id = $_POST['id'];
+        $query = "select * from todutbl where id = $id";
+        $result = mysqli_query($con, $query);
+        $result_Data = array();
+        $row = mysqli_fetch_assoc($result);
+        $result_Data = $row;
+        unlink($id);
+        echo json_encode($result_Data);
+    }
+    else if(isset($_POST['editid'])){ 
+        $id = $_POST['editid'];
+        $title = $_POST['title'];
+        $description = $_POST['description'];
 
-    $query = "UPDATE TODU_TBL SET title='$title',description='$description' WHERE id = $id";
-    if(mysqli_query($con,$query))
-    {
-        echo 1;
-    }
-    else
-    {
-        echo 0;
-    }
+        $query = "UPDATE todutbl SET title='$title',description='$description' WHERE id = $id";
+        if(mysqli_query($con,$query))
+        {
+            echo $query;
+        }
+        else
+        {
+            return $query;
+        }
+}
 ?>
